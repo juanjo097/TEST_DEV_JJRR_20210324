7ad24fa54570cdd4c6af5900318640ee7fee7ab6
juanjo097 <rodriguez.rangel97@gmail.com>
2021-03-25 00:40:53 -0600

    add changes on stored procedures

---
+++ NewsSp/Script1.sql
@@ -0,0 +1,153 @@
+CREATE PROCEDURE dbo.sp_EliminarPersonaFisica
+(@IdPersonaFisica INT)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    DECLARE @ID INT,
+            @ESTATUS VARCHAR(500);
+    BEGIN TRY
+        IF EXISTS
+        (
+            SELECT *
+            FROM dbo.Tb_PersonasFisicas
+            WHERE IdPersonaFisica = @IdPersonaFisica
+                  AND Activo = 1
+        )
+		BEGIN
+			UPDATE dbo.Tb_PersonasFisicas
+			SET Activo = 0
+			WHERE IdPersonaFisica = @IdPersonaFisica;
+		END
+		ELSE
+			BEGIN
+				SELECT @ESTATUS = 'La persona fisica no existe.';
+				THROW 50000, @ERROR, 1;
+		END;
+
+        
+    END TRY
+    BEGIN CATCH
+        PRINT ERROR_MESSAGE();
+        SELECT ERROR_NUMBER() * -1 AS ESTATUS,
+               ISNULL(@ESTATUS, 'Error al actualizar el registro.') AS MENSAJE;
+    END CATCH;
+END;
+GO
+
+
+CREATE PROCEDURE dbo.sp_AgregarPersonaFisica
+(
+    @Nombre VARCHAR(50),
+    @ApellidoPaterno VARCHAR(50),
+    @ApellidoMaterno VARCHAR(50),
+    @RFC VARCHAR(13),
+    @FechaNacimiento DATE,
+    @UsuarioAgrega INT
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    DECLARE @ID INT,
+            @ESTATUS VARCHAR(500);
+    BEGIN TRY
+        IF LEN(@RFC) != 13
+        BEGIN
+            SELECT @ESTATUS = 'El RFC no es válido';
+            THROW 50000, @ESTATUS, 1;
+        END;
+        IF EXISTS
+        (
+            SELECT *
+            FROM dbo.Tb_PersonasFisicas
+            WHERE RFC = @RFC
+                  AND Activo = 1
+        )
+        BEGIN
+            SELECT @ESTATUS = 'El RFC ya existe en el sistema';
+            THROW 50000, @ESTATUS, 1;
+        END;
+
+        INSERT INTO dbo.Tb_PersonasFisicas
+        (
+            FechaRegistro,
+            FechaActualizacion,
+            Nombre,
+            ApellidoPaterno,
+            ApellidoMaterno,
+            RFC,
+            FechaNacimiento,
+            UsuarioAgrega,
+            Activo
+        )
+        VALUES
+        (   GETDATE(),        -- FechaRegistro - datetime
+            NULL,             -- FechaActualizacion - datetime
+            @Nombre,          -- Nombre - varchar(50)
+            @ApellidoPaterno, -- ApellidoPaterno - varchar(50)
+            @ApellidoMaterno, -- ApellidoMaterno - varchar(50)
+            @RFC,             -- RFC - varchar(13)
+            @FechaNacimiento, -- FechaNacimiento - date
+            @UsuarioAgrega,   -- UsuarioAgrega - int
+            1                 -- Activo - bit
+            );
+
+        SELECT @ID = SCOPE_IDENTITY();
+        SELECT @ID AS ESTATUS,
+               'Registro exitoso' AS MENSAJE;
+    END TRY
+    BEGIN CATCH
+        PRINT ERROR_MESSAGE();
+        SELECT ERROR_NUMBER() * -1 AS ESTATUS,
+               ISNULL(@ESTATUS, 'Error al guardar el registro.') AS MENSAJE;
+    END CATCH;
+END;
+GO
+
+CREATE PROCEDURE dbo.sp_ActualizarPersonaFisica
+(
+    @IdPersonaFisica INT,
+    @Nombre VARCHAR(50),
+    @ApellidoPaterno VARCHAR(50),
+    @ApellidoMaterno VARCHAR(50),
+    @RFC VARCHAR(13),
+    @FechaNacimiento DATE,
+    @UsuarioAgrega INT
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    DECLARE @ID INT,
+            @ESTATUS VARCHAR(500);
+    BEGIN TRY
+        IF EXISTS
+        (
+            SELECT *
+            FROM dbo.Tb_PersonasFisicas
+            WHERE IdPersonaFisica = @IdPersonaFisica
+                  AND Activo = 1
+        )
+		BEGIN
+			UPDATE dbo.Tb_PersonasFisicas
+			SET Nombre = @Nombre,
+				ApellidoPaterno = @ApellidoPaterno,
+				ApellidoMaterno = @ApellidoMaterno,
+				RFC = @RFC,
+				FechaNacimiento = @FechaNacimiento
+			WHERE IdPersonaFisica = @IdPersonaFisica;
+
+			SELECT @IdPersonaFisica AS ESTATUS,
+				   'Registro exitoso' AS MENSAJE;
+		END;
+		ELSE
+			BEGIN
+				SELECT @ESTATUS = 'La persona fisica no existe.';
+				THROW 50000, @ESTATUS, 1;
+			END;
+    END TRY
+    BEGIN CATCH
+        PRINT ERROR_MESSAGE();
+        SELECT ERROR_NUMBER() * -1 AS ESTATUS,
+               ISNULL(@ESTATUS, 'Error al actualizar el registro.') AS MENSAJE;
+    END CATCH;
+END;
+GO

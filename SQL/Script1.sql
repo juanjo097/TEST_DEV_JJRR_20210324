f6e7279af1f8b4ba4e81399a87b250b20308b9b6
juanjo097 <rodriguez.rangel97@gmail.com>
2021-03-25 11:23:48 -0600

    add change on sql script

--- SQL/Script1.sql
+++ SQL/Script1.sql
@@ -1,3 +1,39 @@
+IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'PruebaDev1')
+	BEGIN
+		CREATE DATABASE [PruebaDev1]
+	END
+    GO
+USE [PruebaDev1]
+GO
+
+
+CREATE TABLE Tb_PersonasFisicas
+(
+    IdPersonaFisica INT IDENTITY,
+    FechaRegistro DATETIME,
+    FechaActualizacion DATETIME,
+    Nombre VARCHAR(50),
+    ApellidoPaterno VARCHAR(50),
+    ApellidoMaterno VARCHAR(50),
+    RFC VARCHAR(13),
+    FechaNacimiento DATE,
+    UsuarioAgrega INT,
+    Activo BIT
+);
+
+ALTER TABLE Tb_PersonasFisicas
+ADD CONSTRAINT [PK_Tb_PersonasFisicas]
+    PRIMARY KEY (IdPersonaFisica);
+
+ALTER TABLE Tb_PersonasFisicas
+ADD CONSTRAINT [DF_Tb_PersonasFisicas_FechaRegistro]
+    DEFAULT (GETDATE()) FOR FechaRegistro;
+
+ALTER TABLE Tb_PersonasFisicas
+ADD CONSTRAINT [DF_Tb_PersonasFisicas_Activo]
+    DEFAULT (1) FOR Activo;
+GO
+
 CREATE PROCEDURE dbo.sp_EliminarPersonaFisica
 (@IdPersonaFisica INT)
 AS
